4db21c19a535bd25ce99724f6dd86c96f4dd6b2f
Leonardo Machado <julianocanvaju@gmail.com>
2026-07-17 13:07:24 -0300

    chore: remove arquivos indevidos do LeadMaps

--- leadmaps-collector/src/config.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-APP_NAME = "LEADMAPS COLLECTOR"
-VERSION = "1.0"
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-EXPORT_FOLDER = BASE_DIR / "exports"
-LOG_FOLDER = BASE_DIR / "logs"
